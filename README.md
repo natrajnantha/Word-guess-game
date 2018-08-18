9c7895612e7b7687db09d9d12bb6f0e814a7d1b5
Gladiator <Login@DESKTOP-5QFDP32.MRMC.LOC>
2018-08-18 16:25:42 -0700

    version5

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Word-guess-game
 Hangman
+
+ASTRO H A N G M A N Game
+************************
+
+This game is a word guessing game with the theame of Astronomy. All the words and hints used are based on Astronomy. 
+The word maintains an internal database of the Astronomy words. The game logic generates random words and challenges the user to guess the word.
+The game has 2 main jumbotron panels - 
+   Guess panel - This is where the user key is captured and the guess word progress is displayed. This panel also animates a hangman when a non matching key is pressed
+   Stats panel - This panel displays the Total number of tries, number of tries completed and number of tries pending. Also the panel shows all the alphabets and 
+                 strikes out the alphabet that has been used already. If the tried alphabet matches to any letter in the guess word, then the alphabet is struck in 
+                 green font, else in red font. If the user presses the same alphabet, it will still reduce the number of tries indicating that the user has wasted a 
+                 key. 
+The Stats panel also shows a hint button, clicking which the hint will be displayed in the footer area. 
+The footer area also displays if the player has won or lost
+Win event, lose event and hint button click all plays a sound
+Win or Loose will also show the "New Game" button on the navigation bar. When the user clicks the New game, the game will be initialized and the New game button will become invisible again.
+
+
+Notes on pending enhancements - 
+1. Currently there is no logic to suppress a word that has been already used in the game. In future i intend to inculde this logic
+2. Fixing the footer display - If the hint is a large string, the footer currently wraps the hint message. I intended to fix it, but submitting as is for now since its a cosmetic.
+3. To Enhance the code to dynamically animate the hangman. Currently the code uses static coordinates and correspondes to 11 steps only to match the 11 tries. 
+4. Hook to a database after later sessions to get the words and hints database built dynamically
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,14 +42,21 @@ var wordHint = '';
 var resultText = document.getElementById("result-text");
 var resultText = document.getElementById("result-text");
 var playAgainBtn = document.getElementById("playBtn");
+var winSound = new Audio();
+winSound.src = "assets/sounds/tada.wav";
+var loosSound = new Audio();
+loosSound.src = "assets/sounds/loose.wav";
+var hintSound = new Audio();
+hintSound.src = "assets/sounds/Alarm04.wav";
 
 
 //This function accepts 3 arguments - A word, position and a character. The character will get inserted into the word in the nth position 
-//thats passed as the parameter and the resulting word is returned.
+//thats passed as the parameter and the resulting word is returned. 
 function insertGuessChar(word, pos, ch) {
     return word.slice(0, pos) + ch + word.slice(pos + 1);
 }
 
+//This is the initialize function. When a new game is started this function will get called to initialize a new game.
 function initializePlay() {
     screenWord = "                                                ";
     guessWord = "";
@@ -99,6 +106,10 @@ function initializePlay() {
 
 initializePlay();
 
+// This function will draw the hangmam animation inside the canvas. The total number of 11 steps to draw the hangman corrspondes to the 11 tries. So the steps here should 
+// correspond to the number of tries. if the number of tries is increased in the program, the code in this function has to be readjusted to add more steps and draw effect.
+// This is a first version that I tried with learning canvas and in consequtive versions, I mean to improve this code to make it more modulized and to be able to dynamically 
+// handle the draw steps with the number of tries. The line draw coordinates are static for this basic version. 
 function drawHangMan(triesUp) {
     switch (triesUp) {
         case 1:
@@ -183,38 +194,53 @@ function drawHangMan(triesUp) {
 }
 
 
+//Play button is clicked. Call the initialize function.
 document.getElementById("playBtn").onclick = function() {
     initializePlay();
 }
 
+//Hint button is clicked. Display the hint in the footer area
 document.getElementById("hintBtn").onclick = function() {
+    hintSound.play();
     resultText.textContent = "Hint : " + wordHint;
 }
 
 
-// Next, we give JavaScript a function to execute when onkeyup event fires.
+// Below main code utilizes the onkeyup to get the key the user pressed and based on the key, the action is taken on the game. 
+// Actual word is picked up as a random word from the wordstore array and kept in memory. 
+// The guess word initially holds all "underscores" as place holders. The number of characters correspondes to the actual words length.
+// The guess word is stored in the screen word variable as well. Except the screen word variable has a space in between each letters for display purposes in the guess jumbotron panel. 
+// The code senses the user key and matches to the actual word and builds the guess word by inserting the key in the correspondinge guess word position that has a match. 
+// If the user key matches to the word, then the corresponding alphabet is struck out with green font. If no match the alphabet is struck with red font - Both indicating that the 
+// alphabet has been used up. The number of tries remaining is calcualted and displayed in the stats panel as well. 
+// On winning or loosing or clicking the hint, the game plays sound.
 document.onkeyup = function (event) {
     keyTyped = event.key.toLowerCase();
     var matchFound = false;
     if (wordFound || numberofTries >= maxTries) {
-        alert("Refresh page");
+        alert("Refresh page or Click on new game");
     } else {
 
+        // Traverse thru the actualword array and build the guessword if there is a character match
         for (let index = 0; index < actualWord.length; index++) {
             if (actualWord[index] === keyTyped && guessWord[index] !== keyTyped) {
                 guessWord = insertGuessChar(guessWord, index, keyTyped);
                 matchFound = true;
             }
         }
+
+        //The guess word has been uncovered by the user, so set the wordfound flag to true
         if (guessWord === actualWord) {
             wordFound = true;
 
         }
+
         if (!matchFound) {
             numberofTries++;
         }
 
 
+        //Based on the guessword uncovered so far, build the screen word by inserting the charcters with spaces in between for display purposes.
         var j = 0;
         screenWord = "                                                ";
         for (i = 0; i < actualWord.length * 2; i += 2) {
@@ -224,12 +250,15 @@ document.onkeyup = function (event) {
         userText.textContent = screenWord;
     }
 
+    //Set the game stats with number of tries, pending tries.
     var targetDiv = document.getElementById("exhausted");
     targetDiv.textContent = numberofTries;
 
     var targetDiv = document.getElementById("pending");
     targetDiv.textContent = maxTries - numberofTries;
 
+    //Traverse thru the alphabets array to find a match to strike the matching letters in the alphatron panel on screen. This indicates that the 
+    //letter has been used. Green strike if matching or red strike if no match.
     for (let index = 0; index < alphabets.length; index++) {
         if (alphabets[index] === keyTyped.toUpperCase()) {
             var targetDiv = document.getElementById(alphabets[index]);
@@ -244,10 +273,13 @@ document.onkeyup = function (event) {
     }
 
     
+    //Below code determines win or loose and displays the result in the footer area. Also plays a sound accordingly.
     if (wordFound) {
+        winSound.play();
         resultText.textContent = "Awesome !!!, you did it";
         playAgainBtn.setAttribute("class","btn btn-dark d-block");
     } else if (numberofTries === maxTries) { 
+        loosSound.play();
         resultText.textContent = "The actual word is " + actualWord;
         playAgainBtn.setAttribute("class","btn btn-dark d-block");
     }
